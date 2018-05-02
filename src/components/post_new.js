cd2d7849fd4c96afa6e540165ae0da47ffe70c73
Sonicloop <marcm@sonicloop.net>
2018-05-02 16:04:14 -0400

    going to add validation

--- src/components/post_new.js
+++ src/components/post_new.js
@@ -1,13 +1,49 @@
 import React, { Component } from 'react';
+import { Field, reduxForm } from 'redux-form';
 
 class PostsNew extends Component {
+  static renderField(field) {
+    return (
+      <div className="form-group">
+        <label>{field.label}</label>
+        <input
+          className="form-control"
+          type="text"
+          id="title"
+          {...field.input}
+        />
+      </div>
+    );
+  }
+
+
   render() {
     return (
       <div>
-                Post New!
+        <form >
+          <Field
+            label="Title"
+            name="title"
+            component={PostsNew.renderField}
+          />
+          <Field
+            label="Categories"
+            name="categories"
+            component={PostsNew.renderField}
+          />
+          <Field
+            label="Post Content"
+            name="content"
+            component={PostsNew.renderField}
+          />
+
+
+        </form>
       </div>
     );
   }
 }
 
-export default PostsNew;
+export default reduxForm({
+  form: 'PostNewForm',
+})(PostsNew);
